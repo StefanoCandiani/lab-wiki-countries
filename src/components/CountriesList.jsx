f20c0e177896d1271585e9ddf56243ed40281624
Stefano Candiani <stefanocandiani1@MacBook-Air-di-Stefano.local>
2021-02-28 15:43:20 -0500

    Iteration 1.3 - Done

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import TheList from '../countries.json'
+
+const CountriesList = () => {
+
+    return(
+        <div class="col-5" style={{maxHeight: "90vh", overflowY: "scroll"}}>
+            <div className="list-group">
+                {
+                    TheList.map((el) => {
+                        let location = `/${el.cca3}`
+                        return(
+                            <Link className="list-group-item list-group-item-action" exact to={location}>{el.flag} {el.name.official}</Link>
+                        )
+                    })            
+                }                  
+            </div>
+        </div>
+    )
+        
+
+}
+
+export default CountriesList;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,7 @@ const Navbar = () => {
     return(
       <nav class="navbar navbar-dark bg-primary mb-3">
         <div class="container">
-            <h1>Wiki Countries Lab</h1>
-            <Link class="navbar-brand" href="/">WikiCountries</Link>
+            <h1><Link class="navbar-brand" exact to="/">WikiCountries</Link></h1>
         </div>
       </nav>
     )
