b42801e99d86f6b430e4f9f6531067f25f660b89
Stefano Candiani <stefanocandiani1@MacBook-Air-di-Stefano.local>
2021-03-01 18:36:23 -0500

    Iteration 3 - WIP

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -3,14 +3,30 @@ import { Route, Switch, Link } from 'react-router-dom';
 import TheList from '../countries.json'
 
 const CountryDetails = (props) => {
-    console.log(props)
+
+    const checkIndependency = (ind) => {
+        if(ind === true) {
+            return ( <td>Independent</td> ) 
+        } else {
+            return ( <td>Not Independent</td> )
+        }
+    }
+
+    // const checkUN = (isUN) => {
+    //     if(isUN === true) {
+    //         return ( <td>Member of UN</td> ) 
+    //     } else {
+    //         return ( <td>Not a Member of UN</td> )
+    //     }
+    // }
+
     return(
     <div className="col-7">
         <Switch>
         {
             TheList.map((el) => {
                 let thisPath = `/${el.cca3}`
-
+                
                 return (
 
                     <Route exact path={thisPath}>
@@ -26,6 +42,14 @@ const CountryDetails = (props) => {
                                         <td style={{width:"30%"}}>Area</td>
                                         <td>{el.area} km<sup>2</sup></td>
                                     </tr>
+                                    <tr>
+                                        <td style={{width:"30%"}}>Independency Status</td>
+                                        { checkIndependency(el.independent) }
+                                    </tr>
+                                    {/* <tr>
+                                        <td style={{width:"30%"}}>UN Membership</td>
+                                        { checkUN(el.unMember) }
+                                    </tr> */}
                                     <tr>
                                         <td style={{width:"30%"}}>Borders</td>
                                         <td>
